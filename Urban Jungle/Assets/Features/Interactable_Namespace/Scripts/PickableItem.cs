38d8a1c4273c4f560e35309fbe1ea42a9306c58b
Shiro Skrattberg <hikari.hanakimi@gmail.com>
2022-01-18 21:16:55 +0100

    Add pick up mechanism and collider to items

--- Urban Jungle/Assets/Features/Interactable_Namespace/Scripts/PickableItem.cs
+++ Urban Jungle/Assets/Features/Interactable_Namespace/Scripts/PickableItem.cs
@@ -1,21 +1,24 @@
-using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PickableItem : MonoBehaviour
 {
     [SerializeField] private GameObject item;
-    [SerializeField] private GameObject player;
+    [SerializeField] private PickableItems pickableItems;
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.CompareTag("Player"))
         {
             print("Item picked up");
             item.SetActive(false);
-            if (item.name.Contains("Pants"))
+            var datePreparation = collider.gameObject.GetComponent<DatePreparation>();
+            if (pickableItems == PickableItems.NewHairstyle)
             {
-                print("this is pants");
-                
+                datePreparation.SwapHairStyle();
+            }
+            else
+            {
+                datePreparation.PickUpItem(pickableItems, true);
             }
         }
     }
