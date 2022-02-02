d245ff0434e277b98e4e0401a4504cbe70f9e8da
Cansu0 <cansuilhan97@gmail.com>
2022-02-02 19:22:43 +0100

    Update Countdown time and Buttons in Instruction + Pause

--- Urban Jungle/Assets/Features/UI_Namespace/CountdownController.cs
+++ Urban Jungle/Assets/Features/UI_Namespace/CountdownController.cs
@@ -10,7 +10,7 @@ public class CountdownController : MonoBehaviour
     [SerializeField] private Text countdownInfo;
     [SerializeField] private Text countdownTime;
     [SerializeField] private GameEvent onLoadEndMenu;
-    private float _startingTime = 10f;
+    private float _startingTime = 900f;
     private string _timeGrade;
     private bool _triggeredEndMenu;
     
